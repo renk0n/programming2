f10fc454e90d236151993332e96af887484e3eff
renk0n3 <183565255+renk0n@users.noreply.github.com>
2026-04-30 22:27:40 +0900

    Update input prompt for user clarity

--- task4.c
+++ task4.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     int a, b, c;
-    printf("クラスの人数(a)、野球のチーム数(b)、バレーのチーム数(c)を入力してください: ");
+    printf("クラスの人数(a)、野球のチーム数(b)、バレーのチーム数(c)をスペースで区切って入力してください: ");
     scanf("%d %d %d", &a, &b, &c);
     int exclude = a - ((9 * b) + (6 * c));
     printf("参加できない人数: %d人\n", exclude);
