e2ce4a1df42111e38dd0dcc2c6a8ec220fdee554
renk0n3 <183565255+renk0n@users.noreply.github.com>
2026-04-30 14:18:41 +0900

    問題4

---
+++ task4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    printf("クラスの人数(a)、野球のチーム数(b)、バレーのチーム数(c)を入力してください: ");
+    scanf("%d %d %d", &a, &b, &c);
+    int exclude = a - ((9 * b) + (6 * c));
+    printf("参加できない人数: %d人\n", exclude);
+    return 0;
+}
