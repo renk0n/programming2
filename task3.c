e8caaa71c4ce131c3715a45df3cc75e652dd3d63
renk0n3 <183565255+renk0n@users.noreply.github.com>
2026-04-30 22:26:28 +0900

    Update input prompt for fruit purchase quantities

--- task3.c
+++ task3.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     int apple, orange, strawberry;
-    printf("りんご、オレンジ、苺の購入数を,で入力してください: ");
+    printf("りんご、オレンジ、苺の購入数をスペースで区切って入力してください: ");
     scanf("%d %d %d", &apple, &orange, &strawberry);
     int total = (100 * apple) + (88 * orange) + (398 * strawberry);
     printf("合計金額: %d円\n", total);
