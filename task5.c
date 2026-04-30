25fed89585a47f89cb63855d9a41d192f77baa5f
renk0n3 <183565255+renk0n@users.noreply.github.com>
2026-04-30 14:23:32 +0900

    問題5

---
+++ task5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    double base, height;
+    printf("三角形の底辺と高さを実数で入力してください: ");
+    scanf("%lf %lf", &base, &height);
+    
+    // 底辺 × 高さ ÷ 2
+    double area = (base * height) / 2.0;
+    printf("三角形の面積: %f\n", area);
+    
+    return 0;
+}
